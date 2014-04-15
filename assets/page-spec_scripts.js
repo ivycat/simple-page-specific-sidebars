54997fe94e6a182feb9da8acb3e39bf99ef49bde
Patrick Jackson <pjackson@goldenpathsolutions.com>
2014-04-15 12:20:02 -0700

    v2.14.2 Fixed javascript issues on Edit Page meta box.  Basic functions working normally in 3.8.x Treating home page like all other pages for now because code for special treatment of Home page wasn't working.
    Need to reproduce Loop and Home page issues to troubleshoot.
    Discovered issue when switching to another (child) theme: widgets and sidebars are rearranged.

--- assets/page-spec_scripts.js
+++ assets/page-spec_scripts.js
@@ -1,4 +1,31 @@
+ 
+ /**
+  * page-spec_scripts.js
+  * 
+  * Contains javascript for managing content hiding behaviors in 
+  * Page Specific Sidebars WP plugin.
+  * 
+  * Updated: Apr 14, 2014  Patrick Jackson
+  * copyright 2014 IvyCat Web Services
+  */
+ 
+ 
+ 
  jQuery( 'document' ).ready( function( $ ){
+     
+     //Minor performance boost by storing jQuery searches, and pruning searches
+     $iscustom = $('#iscustom');                                            //"use custom sidebar" checkbox
+     $custom_sidebar = $iscustom.parent().find('.custom-sidebar');            //hidden content ul
+     
+     $grpselect = $custom_sidebar.find( '.grpselect' );                     //"use existing" radio buttons
+     $groupsb = $custom_sidebar.find('#groupsb');                           //"use existing" radio button
+     $existing_sidebars = $custom_sidebar.find( '.existing-sidebars' );     //hidden content ul
+     
+     $addrplce = $custom_sidebar.find( '#addrplce' );                       //"add or replace sidebar" checkbox
+     $sidebar_add = $custom_sidebar.find( '.sidebar-add' );                 //hidden content ul
+     
+     
+     
     $( '#page-specific-sidebar-settings .top-menu li a' ).click( function(){
         var toshow = $( this ).attr( 'href' ).replace( '#', '' );
         $( '.top-menu li' ).removeClass( 'current-menu-tab' );
@@ -7,13 +34,43 @@
         $( '.' + toshow ).show().addClass( 'current-tab' );
         return false;
     } );
-	$( '#addrplce' ).click( function(){
-		$( '.sidebar-add' ).toggle();
-	});
-	$( '#iscustom' ).click( function(){
-		$( '.custom-sidebar' ).toggle();	
-	});
-	$( '.grpselect' ).change( function(){
-		$( '.existing-sidebars' ).toggle();	
-	});
+    
+    
+    
+    
+    //checkbox state may differ depending on browser when page is refreshed,
+    //so need to explicitely initialize checkbox/hidden states
+    if ( $iscustom.prop('checked') ){
+        $custom_sidebar.show();
+        
+        if ( $groupsb.prop('checked') ){
+            $existing_sidebars.show();
+        } else {
+            $existing_sidebars.hide();
+        }
+        
+        if ( $addrplce.prop('checked') ){
+            $sidebar_add.show();
+        } else {
+            $sidebar_add.hide();
+        }
+        
+    } else {
+        $custom_sidebar.hide();
+    }
+    
+    //toggle behavior on checkboxes...
+    $iscustom.change( function(){
+        $custom_sidebar.toggle();	
+    });
+    
+    $grpselect.change( function(){
+	$existing_sidebars.toggle();	
+    });
+        
+     $addrplce.change( function(){
+        $sidebar_add.toggle();
+    });
+
+    
 });
--- page-specific-sidebars.php
+++ page-specific-sidebars.php
@@ -7,11 +7,11 @@
  *  Author: IvyCat Web Services
  *  Author URI: http://www.ivycat.com
  *  Version: 2.14.2
-  *  License: GNU General Public License v2.0
+ *  License: GNU General Public License v2.0
  *  License URI: http://www.gnu.org/licenses/gpl-2.0.html
  
  ------------------------------------------------------------------------
-    Simple Page Specific Sidebars, Copyright 2012 IvyCat, Inc. (admins@ivycat.com)
+    Simple Page Specific Sidebars, Copyright 2014 IvyCat, Inc. (admins@ivycat.com)
     
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -36,15 +36,16 @@ class DGPageSidebarCustom{
     
     protected $home_id;
     protected $widget_name;
+    protected $my_page_id; //grab the page id and keep it handy
     
     public function __construct(){
         self::set_opts();
-        add_filter( 'plugin_action_links_'. plugin_basename( __FILE__ ), array( &$this, 'plugin_action_links' ), 10, 4 );
-        add_action( 'widgets_init', array( &$this, 'build_sidebars' ) );
-        add_action( 'admin_init', array( &$this, 'add_page_meta_box' ) );
-        add_action( 'save_post' , array( &$this, 'save_custom_page_meta' ) );
-        add_filter( 'sidebars_widgets', array( &$this, 'hijack_sidebar' ) );
-        add_action('admin_menu', array(&$this, 'options_page_init') );
+        add_filter( 'plugin_action_links_'. plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ), 10, 4 );
+        add_action( 'widgets_init', array( $this, 'build_sidebars' ) );
+        add_action( 'admin_init', array( $this, 'add_page_meta_box' ) );
+        add_action( 'save_post' , array( $this, 'save_custom_page_meta' ) );
+        add_filter( 'sidebars_widgets', array( $this, 'hijack_sidebar' ) );
+        add_action( 'admin_menu', array($this, 'options_page_init') );
     }
     
 /**
@@ -102,7 +103,7 @@ class DGPageSidebarCustom{
         add_meta_box(
             'custompageopt',
             'Custom Page Options',
-            array( &$this, 'custom_page_meta' ),
+            array( $this, 'custom_page_meta' ),
             'page',
             $location ,
             $priority
@@ -128,42 +129,42 @@ class DGPageSidebarCustom{
                         <li>
                             <input id="customsb" class="grpselect" type="radio" name="customsb" value="custom"<?php echo ( !$sb_group ) ? ' checked="checked"' : ''; ?>/>
                             <label for="customsb">Custom Sidebar </label>
-			</li>
-			<li>
+                        </li>
+                        <li>
                             <input id="groupsb" class="grpselect" type="radio" name="customsb" value="group"<?php echo ( $sb_group ) ? ' checked="checked"' : ''; ?>/>
                             <label for="groupsb">Use Existing Sidebar </label>
-                                <ul class="existing-sidebars<?php echo ( !$sb_group ) ? ' hidden-h' : ''; ?>">
-                                    <li>
-                                        <?php if( is_array( $wp_registered_sidebars ) ): ?>
-                                            <select id="primary-slug" name="primary_sidebar_slug">
-                                                <?php foreach( $wp_registered_sidebars as $slug => $sidebar ):?>
-                                                    <option value="<?php echo $slug . '"' . selected( $slug, $sb_group, false ); ?>">
-                                                        <?php  echo $sidebar['name']; ?>
-                                                    </option>
-                                                <?php endforeach; ?>
-                                            </select>
-                                        <?php else: ?>
-                                            It appears you have no sidebars registered with this theme.
-                                        <?php endif; ?>
-                                    </li>
-				</ul>
-			</li>
+                            <ul class="existing-sidebars<?php echo ( !$sb_group ) ? ' hidden-h' : ''; ?>">
+                                <li>
+                                    <?php if( is_array( $wp_registered_sidebars ) ): ?>
+                                        <select id="primary-slug" name="primary_sidebar_slug">
+                                            <?php foreach( $wp_registered_sidebars as $slug => $sidebar ):?>
+                                                <option value="<?php echo $slug . '"' . selected( $slug, $sb_group, false ); ?>">
+                                                    <?php  echo $sidebar['name']; ?>
+                                                </option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    <?php else: ?>
+                                        It appears you have no sidebars registered with this theme.
+                                    <?php endif; ?>
+                                </li>
+                            </ul>
+                        </li>
                         <li class="add-replace">
                             <input type="checkbox" id="addrplce" name="add2sidebar" value="add2chk"<?php echo $add2chk; ?>/>
-                            <label for="addrplce">Add to sidebar rather than replace: <label>
+                            <label for="addrplce">Add to sidebar rather than replace: </label>
                             <ul class="sidebar-add <?php echo $add2sb ? '' : ' hidden-h'; ?>">
                                 <li>
                                     <label>
                                         <input type="radio" name="pre-append" value="prepend"<?php echo $prepend ? ' checked="checked"' : ''  ?>/>
                                         Prepend Sidebar (before)
                                     </label>
-				</li>
-				<li>
+				                </li>
+				                <li>
                                     <label>
                                         <input type="radio" name="pre-append" value="append"<?php echo !$prepend ? ' checked="checked"' : ''  ?>/>
 					Append Sidebar (after)
                                     </label>
-				</li>
+				                </li>
                             </ul>
                         </li>
                     </ul>
@@ -174,94 +175,134 @@ class DGPageSidebarCustom{
     }
     
     
-    public function save_custom_page_meta(){
+    public function save_custom_page_meta( $post_id ){
         if ( defined('DOING_AJAX') ) return;
         global $post;
-        if (array_key_exists('customsb', $_POST)){
+        
+        //Set whether to use custom sidebar
+        if ( isset($_POST['is-custom']) ){
+            update_post_meta( $post_id, 'is_custom', $_POST['is-custom'] );
+        } else {
+            delete_post_meta( $post_id, 'is_custom');
+        }
+        
+        //set whether to use an existing sidebar (group) or not (custom)
+        if ( isset($_POST['customsb'])){
             $sb_group = ( $_POST['customsb'] == 'group' ) ? $_POST['primary_sidebar_slug'] : false;
-            if (array_key_exists('is-custom', $_POST)){
-                update_post_meta( $post->ID, 'is_custom', $_POST['is-custom'] );
-            }
-            if (array_key_exists('add2sidebar', $_POST)){
-                update_post_meta( $post->ID, 'add2sidebar', $_POST['add2sidebar'] );
-            }
-            update_post_meta( $post->ID, 'prepend_to_sidebar', $_POST['pre-append'] );
-            update_post_meta( $post->ID, 'use_sidebar_group', $sb_group );
+            update_post_meta( $post_id, 'use_sidebar_group', $sb_group );
+        }
+        if ( isset($_POST['add2sidebar']) ){
+            update_post_meta( $post_id, 'add2sidebar', $_POST['add2sidebar'] );
+        } else {
+            delete_post_meta( $post_id, 'add2sidebar');
+        }
+        if (isset( $_POST['pre-append'] ) ){
+            update_post_meta( $post_id, 'prepend_to_sidebar', $_POST['pre-append'] );
+        } else {
+            delete_post_meta( $post_id, 'prepend_to_sidebar' );
         }
     }
     
     public function build_sidebars(){
+        
+        global $_wp_sidebars_widgets;
+        
         $pages = self::get_pages();
         $stop = count( $pages );
         $count = 0;
-        foreach( $pages as $page ):
-			$sb_group = get_post_meta( $page->ID, 'use_sidebar_group', true );
-			$count++; 
-			if( $count <= $stop  && !$sb_group ){
-				$args = array(
-				'name'          => __( $page->post_title . ' Sidebar' ),
-				'id'            => 'page-sidebar-'. $page->ID ,
-				'description'   => '',
-				'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
-				'after_widget' => '</div></div>',
-				'before_title' => '<h3 class="widget-title">',
-				'after_title' => '</h3>' );
-				
-			   register_sidebar( $args ); 
-			}
-	    endforeach;
+        foreach( $pages as $page ){
+            
+            $sb_group = get_post_meta( $page->ID, 'use_sidebar_group', true );
+            $count++; 
+            if( $count <= $stop  && !$sb_group ){
+                $args = array(
+                'name'          => __( $page->post_title . ' Sidebar' ),
+                'id'            => 'page-sidebar-'. $page->ID ,
+                'description'   => '',
+                'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-wrap widget-inside">',
+                'after_widget' => '</div></div>',
+                'before_title' => '<h3 class="widget-title">',
+                'after_title' => '</h3>' );
+
+                register_sidebar( $args );
+                
+            }
+            
+        }
     }
      
     public function hijack_sidebar( $sidebars=array() ){
-		if( did_action( 'sidebars_widgets' ) == 1 )
-			return $sidebars;
+        
+        //Not sure what this is for? Is there a sidebars_widgets action? PJ 4/14/2014
+       if( did_action( 'sidebars_widgets' ) == 1 )
+            return $sidebars;
+        
         global $wp_registered_widgets, $wp_registered_sidebars, $_wp_sidebars_widgets, $post;
         $sidebar_title = apply_filters( 'page-sidebar-title' , $this->widget_name );
 		
+        
+        //Only operate on front-end "Page" type posts
         if( !is_page() || is_admin() ) return $sidebars;
+        
+        //collect the plugin parameters
         $is_custom = get_post_meta( $post->ID, 'is_custom', true );
         $add2sidebar = get_post_meta( $post->ID, 'add2sidebar', true );
-		$prepend = ( get_post_meta( $post->ID, 'prepend_to_sidebar', true ) == 'prepend' ) ? true : false;
-		$sb_group = ( $group = get_post_meta( $post->ID, 'use_sidebar_group', true ) ) ? $group : false;
+        $prepend = ( get_post_meta( $post->ID, 'prepend_to_sidebar', true ) == 'prepend' ) ? true : false;
+        $sb_group = ( $group = get_post_meta( $post->ID, 'use_sidebar_group', true ) ) ? $group : false;
         
-		if( $is_custom != 'y'){
+        //stop if the custom sidebar box wasn't checked
+	if( $is_custom != 'y')
             return $sidebars;
-        }else{
-			if( $sb_group ):
-				//$sidebar_term = ();
-				$sidebar_term = $sb_group;
-			else:
-				$sidebar_term = ( is_front_page() ) ? 'page-sidebar-' . self::home_pg_id() : 'page-sidebar-' . $post->ID;
-			endif;
-			
-            $sidebars_widgets = $_wp_sidebars_widgets;
+        
+        //use the selected existing sidebar, or get the custom sidebar for this page
+        if( $sb_group ){
+            $sidebar_term = $sb_group;
+        } else {
+            //$sidebar_term = ( is_front_page() ) ? 'page-sidebar-' . self::home_pg_id() : 'page-sidebar-' . $post->ID;
+            $sidebar_term = "page-sidebar-{$post->ID}";
+        }
+
+        $sidebars_widgets = $_wp_sidebars_widgets;
+        
+        //if the widgets we want are missing, bail
+        if( !array_key_exists( $sidebar_term, $sidebars_widgets) || count($_wp_sidebars_widgets[$sidebar_term]) < 1 ){
             
-            if( !array_key_exists( $sidebar_term, $sidebars_widgets) || count($_wp_sidebars_widgets[$sidebar_term]) < 1 ){
-				
-                return $sidebars; 
-            
-			}else{
+            return $sidebars; 
+
+        } else{
+
+            //make sure widgets aren't corrupt (WP3+)
+            if( $sidebars_widgets['array_version'] != 3  )
+                    return $sidebars;
+
+            //handle whether to add to existing sidebar
+            if( $add2sidebar ){
                 
-				if( $sidebars_widgets['array_version'] != 3  )
-					return $sidebars;
+                $add_sidebar = (array)$sidebars[$sidebar_title];
                 
-				if( $add2sidebar ){
-					$add_sidebar = (array)$sidebars[$sidebar_title];
-					if( is_array( $_wp_sidebars_widgets[$sidebar_term] ) ){
-						$sidebars[$sidebar_title] = ( $prepend )
-							? array_merge( $_wp_sidebars_widgets[$sidebar_term], $add_sidebar )
-							: array_merge( $add_sidebar, $_wp_sidebars_widgets[$sidebar_term] );
-					}else{
-						$sidebars[$sidebar_title] = $add_sidebar;
-					}
+                //there must be more than one widget if they're going to be added
+                //together.  If there's more than one, they'll be stored as an array
+                if( is_array( $_wp_sidebars_widgets[$sidebar_term] ) ){
+                    
+                    $sidebars[$sidebar_title] = ( $prepend )
+                        ? array_merge( $_wp_sidebars_widgets[$sidebar_term], $add_sidebar )
+                        : array_merge( $add_sidebar, $_wp_sidebars_widgets[$sidebar_term] );
                     
                 }else{
-					
-                   $sidebars[$sidebar_title] = $_wp_sidebars_widgets[$sidebar_term];
-				   
+                    
+                    //if there's only one sidebar, then use it
+                    $sidebars[$sidebar_title] = $add_sidebar;
                 }
-                return $sidebars;
+
+            }else{
+                
+                //Here is where we replace the primary sidebar if we're
+                //not adding to it.
+                $sidebars[$sidebar_title] = $_wp_sidebars_widgets[$sidebar_term];
+
             }
+            
+            return $sidebars;
         }
     }
     
@@ -273,14 +314,14 @@ class DGPageSidebarCustom{
      *  Often times the Homepage has posts on it, dictated by the template.  For some reason, this sets the post->ID for the page as the last post listed on the page
      *  which keeps this plugin from inserting the correct sidebar.  This is the fix.
      */
-    protected function home_pg_id(){
+    /*protected function home_pg_id(){
         $home_slug = 'home';
         $home_slug = apply_filters( 'page-sidebar-homeslug', $home_slug );
         $pg = get_page_by_path( $home_slug );
         if ($pg){
             return $pg->ID;
         }
-    }
+    }*/
     
     protected function get_pages(){
         $pages = get_posts( array( 'post_type'=>'page', 'numberposts'=>-1, 'orderby'=>'post_title', "meta_key"=>"is_custom", "meta_value"=>"y" ) );
